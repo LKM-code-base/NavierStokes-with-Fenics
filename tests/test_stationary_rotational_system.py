144032584cd6ec8c0486ae3a29d9f5b646422ff3
schmaikos <84124267+schmaikos@users.noreply.github.com>
2021-07-01 19:20:31 +0200

    Update test_stationary_rotational_system.py
    Need to replace setup for the coriolis and euler force into solver base (dependency of sol_v)

--- tests/test_stationary_rotational_system.py
+++ tests/test_stationary_rotational_system.py
@@ -36,23 +36,23 @@ class RotationalProblem(StationaryProblem):
     Omega = dlfn.Constant(1.0)
     Alpha = dlfn.Constant(1.0)
     
-    def set_coriolis_force(self):
-        assert isinstance(Omega, dlfn.Constant)
-        if self._mesh.geometry().dim() is 2:
-            assert len(Omega) == 1
-            set._coriolis_force = 2 * dlfn.as_vector((-Omega * sol_v[1], Omega * sol_v[0]))  
-        else:
-            assert len(Omega) == 3
-            self._coriolis_force = 2 * dlfn.cross(Omega, sol_v)
+    #def set_coriolis_force(self):
+    #    assert isinstance(Omega, dlfn.Constant)
+    #    if self._mesh.geometry().dim() is 2:
+    #        assert len(Omega) == 1
+    #        set._coriolis_force = 2 * dlfn.as_vector((-Omega * sol_v[1], Omega * sol_v[0]))  
+    #    else:
+    #        assert len(Omega) == 3
+    #        self._coriolis_force = 2 * dlfn.cross(Omega, sol_v)
             
-    def set_euler_force(self):
-        assert isinstance(Alpha, dlfn.Constant)
-        if self._mesh.geometry().dim() is 2:
-            assert len(Alpha) == 1
-            set._euler_force = dlfn.as_vector((-Alpha * sol_v[1], Alpha * sol_v[0]))
-        else:
-            assert len(Alpha) == 3
-            self._euler_force = dlfn.cross(Alpha, sol_v)
+    #def set_euler_force(self):
+    #    assert isinstance(Alpha, dlfn.Constant)
+    #    if self._mesh.geometry().dim() is 2:
+    #        assert len(Alpha) == 1
+    #        set._euler_force = dlfn.as_vector((-Alpha * sol_v[1], Alpha * sol_v[0]))
+    #    else:
+    #        assert len(Alpha) == 3
+    #        self._euler_force = dlfn.cross(Alpha, sol_v)
 
 def test_rotational():
     rotational_flow = RotationalProblem(25)
