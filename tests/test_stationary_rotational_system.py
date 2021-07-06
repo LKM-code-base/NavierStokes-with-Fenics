44f989c81840e33f3f38b4afdfbf96592e5b1704
SeMardt <84124421+SeMardt@users.noreply.github.com>
2021-07-06 13:25:52 +0200

    Added rotational couette flow test
    Exterior bc must still be adjusted

--- tests/test_stationary_rotational_system.py
+++ tests/test_stationary_rotational_system.py
@@ -12,14 +12,14 @@ from grid_generator import HyperRectangleBoundaryMarkers
 dlfn.set_log_level(20)
 
 
-class RotationalProblem(StationaryProblem):
+class RotationalCavityProblem(StationaryProblem):
     def __init__(self, n_points, main_dir=None):
         super().__init__(main_dir)
 
         self._n_points = n_points
         self._problem_name = "Rotational"
 
-        self.set_parameters(Re=10.0, Ro=1.0, Omega=dlfn.Constant(50), Alpha=dlfn.Constant(0))
+        self.set_parameters(Re=10.0, Ro=0.01, Omega=dlfn.Constant(50), Alpha=dlfn.Constant(0))
 
     def setup_mesh(self):
         # create mesh
@@ -31,11 +31,38 @@ class RotationalProblem(StationaryProblem):
                      (VelocityBCType.no_slip, HyperCubeBoundaryMarkers.right.value, None),
                      (VelocityBCType.no_slip, HyperCubeBoundaryMarkers.bottom.value, None),
                      (VelocityBCType.constant, HyperCubeBoundaryMarkers.top.value, (1.0, 0.0)))
+        
+        
+class RotationalCouetteFlow(StationaryProblem):
+    def __init__(self, n_refinements, radii, main_dir=None):
+        super().__init__(main_dir)
+
+        assert isinstance(radii, (list, tuple)) and len(radii) == 2, "radii must be tuple of length 2"
+        self._radii = radii
+        self._n_refinements = n_refinements
+        self._problem_name = "RotationalCouette"
+
+        self.set_parameters(Re=10.0, Ro=0.01, Omega=dlfn.Constant(50), Alpha=dlfn.Constant(0))
+
+    def setup_mesh(self):
+        # create mesh
+        self._mesh, self._boundary_markers = spherical_shell(2, self._radii, self._n_refinements)
+
+    def set_boundary_conditions(self):
+        # velocity boundary conditions
+        self._bcs = ((VelocityBCType.no_slip,SphericalAnnulusBoundaryMarkers.interior_boundary.value, None),
+                     (VelocityBCType.constant, SphericalAnnulusBoundaryMarkers.exterior_boundary.value, (1.0, 0.0)))
+        
  
-def test_rotational():
-    rotational_flow = RotationalProblem(25)
-    rotational_flow.solve_problem()
+def test_rotational_cavity():
+    rotational_cavity_flow = RotationalCavityProblem(25)
+    rotational__cavity_flow.solve_problem()
+    
+def test_rotational_couette():
+    rotational_couette_flow = RotationalCouetteFlow(3, (1.0, 0.25))
+    rotational__couette_flow.solve_problem()
 
 
 if __name__ == "__main__":
-    test_rotational()
+    test_rotational_cavity()
+    test_rotational_couette()
