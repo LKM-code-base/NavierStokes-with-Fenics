44e7c15557d2db1ef55f02dd1a7895e9b346fb7e
Sebastian Glane <glane@tu-berlin.de>
2021-07-29 13:48:19 +0200

    Added class handling coefficients

---
+++ tests/test_auxiliary_classes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from auxiliary_classes import EquationCoefficients
+
+def test_equation_coefficients():
+    eq01 = EquationCoefficients(Reynolds=50.0)
+    print(eq01)
+    eq01.Fr = 10.0
+    _ = eq01.equation_coefficients
+    print(eq01)
+    eq01.Ek = 25.0
+    _ = eq01.equation_coefficients
+    print(eq01)
+    _ = eq01.Fr
+    _ = eq01.Ek
+    _ = eq01.Re
+
+    eq02 = EquationCoefficients()
+    print(eq02)
+    eq02.Ro = 1.0
+    _ = eq02.equation_coefficients
+    print(eq02)
+    eq02.Ek = 25.0
+    _ = eq02.equation_coefficients
+    print(eq02)
+    eq02.Fr = 10.0
+    _ = eq02.equation_coefficients
+    print(eq02)
+    _ = eq01.Fr
+    _ = eq01.Ek
+    _ = eq01.Ro
+
+if __name__ == "__main__":
+    test_equation_coefficients()
